f1f7ab8f71bf7e09f87ecb50e0d437be544d86c3
ilya5015 <ilya50152@yandex.ru>
2022-09-12 20:00:24 +0300

    Added jwt and authorization api(not fully working yet). Added registration api with jwt

--- controllers/user_auth_data_controller.js
+++ controllers/user_auth_data_controller.js
@@ -1,28 +1,88 @@
 import { ApiError } from "../error/ApiError.js";
-import { user_auth_data } from "../models/models.js";
+import { user_auth_data, user_data } from "../models/models.js";
+import * as bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const generateJwt = (id, login, password) => {
+  const token = jwt.sign(
+    {
+      id,
+      login,
+      password,
+    },
+    process.env.SECRET_KEY,
+    { expiresIn: "24h" }
+  );
+  return token;
+};
 
 class UserAuthDataController {
-  async registration(req, res) {
+  async registration(req, res, next) {
     try {
-      const { login, password } = req.body;
-      console.log("REQUEST BODY IS", req.body);
+      const { login, password, name, email } = req.body;
+      if (!login || !password) {
+        return next(ApiError.badRequest("Некорректный login или password"));
+      }
+      const candidate = await user_auth_data.findOne({ where: { login } });
+      if (candidate) {
+        return next(
+          ApiError.badRequest("Пользователь с таким именем существует")
+        );
+      }
+      const hashPassword = await bcrypt.hash(password, 5);
       const userAuthData = await user_auth_data.create({
         login: login,
-        password: password,
+        password: hashPassword,
       });
-      return res.json(userAuthData);
+      const userData = await user_data.create({
+        id: userAuthData.id,
+        name,
+        email,
+      });
+      const token = generateJwt(
+        userAuthData.id,
+        userAuthData.login,
+        userAuthData.password
+      );
+      return res.json({ token });
     } catch (e) {
       next(ApiError.badRequest(e.message));
     }
   }
 
-  async getUserData(req, res) {
-    return res.json("getUserData");
+  async login(req, res, next) {
+    const { login, password } = req.body;
+    const candidate = await user_auth_data.findOne({ where: { login } });
+    if (!candidate) {
+      return next(ApiError.internal("Пользователь с таким login не найден"));
+    }
+    let comparePassword = bcrypt.compareSync(password, candidate.password);
+    if (!comparePassword) {
+      return next(ApiError.internal("Неверный password"));
+    }
+    const token = generateJwt(candidate.id, candidate.password);
+    return res.json({ token });
   }
 
-  async getUsersData(req, res) {
-    const users = await user_auth_data.findAll();
-    return res.json(users);
+  async getUserData(req, res, next) {
+    try {
+      console.log("req query is", req.params);
+      const { id } = req.params;
+      let user;
+      user = await user_auth_data.findOne({ where: { id } });
+      return res.json(user);
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
+    }
+  }
+
+  async getUsersData(req, res, next) {
+    try {
+      const users = await user_auth_data.findAll();
+      return res.json(users);
+    } catch (e) {
+      next(ApiError.badRequest(e.message));
+    }
   }
 }
 
--- routes/user_auth_data.js
+++ routes/user_auth_data.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { userAuthDataController } from "../controllers/user_auth_data_controller.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
 
 export const userAuthDataRouter = new Router();
 
-userAuthDataRouter.get("/user", userAuthDataController.getUserData);
+userAuthDataRouter.get("/user/:id", userAuthDataController.getUserData);
 userAuthDataRouter.get("/users", userAuthDataController.getUsersData);
 userAuthDataRouter.post("/registration", userAuthDataController.registration);
+userAuthDataRouter.post("/login", authMiddleware, userAuthDataController.login);
